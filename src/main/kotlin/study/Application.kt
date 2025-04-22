f7210b324436045b664378f3dd461a205ddafd45
EUNJI <ejlee0005@gmail.com>
2025-04-22 14:27:17 +0900

    feat:  3단계
    - RSS 피드 10분마다 주기적으로 확인
    - 새 게시글 콘솔 출력
    - 기존 기능 유지

--- src/main/kotlin/study/Application.kt
+++ src/main/kotlin/study/Application.kt
@@ -1,37 +1,68 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import study.Channel
 import study.Post
+import java.time.Duration
 import java.time.format.DateTimeFormatter
-import kotlin.collections.sortedByDescending
 import kotlin.collections.take
 
-fun main() =
+private val channels =
+    listOf(
+        Channel("우아한 기술 블로그", "https://techblog.woowahan.com/feed"),
+        Channel("카카오 기술 블로그", "https://tech.kakao.com/blog/feed"),
+        Channel("컬리 기술 블로그", "https://helloworld.kurly.com/feed"),
+    )
+
+private var prevPosts: List<Post> = emptyList()
+private var currentPosts: List<Post> = emptyList()
+
+fun main() {
     runBlocking {
-        val channels =
-            listOf(
-                Channel("우아한 기술 블로그", "https://techblog.woowahan.com/feed"),
-                Channel("카카오 기술 블로그", "https://tech.kakao.com/blog/feed"),
-                Channel("컬리 기술 블로그", "https://helloworld.kurly.com/feed"),
-            )
+        launch(Dispatchers.IO) {
+            while (isActive) {
+                delay(Duration.ofMinutes(10).toMillis())
+                updatePosts()
+            }
+        }
 
-        while (true) {
-            println("검색어를 입력하세요 (없으면 전체 출력):")
-            val keyWord = readln()
-            val posts =
-                channels.posts().filter { keyWord in it.title }
-                    .sortedByDescending { it.pubDate }
-                    .take(8)
-
-            val result =
-                posts.mapIndexed { idx, post ->
-                    "[${idx + 1}] ${post.title} (${post.pubDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}) - ${post.link}\n"
-                }.joinToString("")
-            println(result)
+        launch {
+            updatePosts()
+            while (isActive) {
+                println("검색어를 입력하세요 (없으면 전체 출력):")
+                val keyword = readln()
+                val filtered =
+                    currentPosts.filter { keyword in it.title }
+                        .sortedByDescending { it.pubDate }
+                        .take(10)
+                val result =
+                    filtered.mapIndexed { idx, post ->
+                        "[${idx + 1}] ${post.title} (${post.pubDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}) - ${post.link}\n"
+                    }.joinToString("")
+                println(result)
+            }
         }
     }
+}
+
+private suspend fun updatePosts() {
+    currentPosts = channels.posts()
+    if (prevPosts.isNotEmpty()) {
+        val result =
+            currentPosts.filterNot { current ->
+                prevPosts.any { prev -> current.title != prev.title }
+            }.map { post ->
+                "[NEW] ${post.title} (${post.pubDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}) - ${post.link}\n"
+            }
+        if (result.isNotEmpty()) println(result)
+    }
+    prevPosts = currentPosts
+}
 
 private suspend fun List<Channel>.posts(): List<Post> {
     return coroutineScope {
--- src/main/kotlin/study/RSSModel.kt
+++
@@ -1,5 +0,0 @@
-package study
-
-import java.util.Date
-
-data class RSSModel(val title: String, val link: String, val pubDate: Date)
